15e94226b49fc89afef12e952dd8aba1cfa4b1a9
Pradhuman Shrestha <nirvanaatjapan13@gmail.com>
2018-12-12 22:38:54 +0900

    every thing is finished

--- blog/models.py
+++ blog/models.py
@@ -9,6 +9,16 @@ from django.db import models
 
 class Blog(models.Model):
     title = models.CharField(max_length=100)
-    pub_date = models.DateTimeField('Published Date')
+    author = models.CharField(max_length=100)
+    pub_date = models.DateTimeField()
     b_content = models.TextField()
     image = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.title
+
+    def shortDetails(self):
+        return self.b_content[:50]
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%B %e %Y')
